9b63e82f0c4822891b6317f70d5b5be4cb2f8739
Guillaume Felley <guillaume@felley.io>
2020-09-15 14:47:01 +0200

    Clean up exports

--- packages/geb-admin/src/index.ts
+++ packages/geb-admin/src/index.ts
@@ -1,3 +1,13 @@
 import { GebAdmin } from './geb-admin'
+import * as adminContracts from '@reflexer-finance/geb-admin-api'
+import { Geb, GebErrorTypes, GebProxyActions, utils, contracts } from 'geb.js'
 
-export { GebAdmin }
+export {
+    GebAdmin,
+    adminContracts,
+    Geb,
+    GebErrorTypes,
+    GebProxyActions,
+    utils,
+    contracts,
+}
--- packages/geb/src/index.ts
+++ packages/geb/src/index.ts
@@ -3,5 +3,6 @@ import { GebProxyActions } from './proxy-action'
 import { Geb } from './geb'
 
 import * as utils from './utils'
+import * as contracts from '@reflexer-finance/geb-contract-api'
 
-export { Geb, GebErrorTypes, GebProxyActions, utils }
+export { Geb, GebErrorTypes, GebProxyActions, utils, contracts }
