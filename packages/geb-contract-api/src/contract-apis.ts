03478c6cb307845e64fb08122c6dc436029b5ebd
Guillaume Felley <guillaume@felley.io>
2021-12-06 15:50:16 +0100

    nit

--- packages/geb-contract-api/src/contract-apis.ts
+++ packages/geb-contract-api/src/contract-apis.ts
@@ -119,7 +119,7 @@ export class ContractApis {
         this.uniswapPairCoinEth = new UniswapV2Pair(addressList.GEB_COIN_UNISWAP_POOL, this.chainProvider)
         this.merkleDistributorFactory = new MerkleDistributorFactory(addressList.MERKLE_DISTRIBUTOR_FACTORY, this.chainProvider)
         this.coinNativeUniswapSaviour = new NativeUnderlyingUniswapSafeSaviour(addressList.GEB_COIN_ETH_UNISWAP_V2_POOL_SAVIOUR, this.chainProvider)
-        this.curveV1MaxSafeSaviour = new CurveV1MaxSafeSaviour(addressList.XXXXXX)
+        this.curveV1MaxSafeSaviour = new CurveV1MaxSafeSaviour(addressList.GEB_COIN_CURVE_V1_MAX_SAVIOUR, this.chainProvider)
         this.saviourCRatioSetter = new SaviourCRatioSetter(addressList.GEB_SAVIOUR_CRATIO_SETTER, this.chainProvider)
         this.saviourUniswapLiquidityManager = new UniswapLiquidityManagerLike(addressList.GEB_UNISWAP_SAVIOUR_LIQUIDITY_MANAGER, this.chainProvider)
         this.uniswapV3PairCoinEth = new UniswapV3Pool(addressList.GEB_COIN_UNISWAP_V3_POOL, this.chainProvider)
